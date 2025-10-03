c483e7556c10579c730ba6682a2224f4ba24973f
mahal <mahal@kumaran1415>
2025-10-03 16:24:17 +0530

    commit

---
+++ assignment/src/com/tnsif/assignmentone/assignment2/MainApp.java
@@ -0,0 +1,15 @@
+package com.tnsif.assignmentone.assignment2;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Student s1 = new Student();
+
+        // Creating Commission object
+        Commission emp = new Commission();
+        emp.acceptDetails();
+        emp.calculateCommission();
+
+	}
+
+}
